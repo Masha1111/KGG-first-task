97d55c553e42c1b0a4f55e2d48e1a80415bddec2
Masha1111 <sowix@yandex.ru>
2019-04-08 23:52:03 +0500

    ordinate axis done

--- firstTask.py
+++ firstTask.py
@@ -2,36 +2,38 @@ from tkinter import *
 import math
 
 
+# вычисление значения функции
 def func(x):
+    # return math.sin(x)
     # return x*math.sin(x*x)
     return x*x
     # return x
 
 
-def max_min(a, b):
-    y_0 = None
+# нахождение максимального и минимального значения функции
+def max_min():
     ymin = ymax = func(a)
     for xx in range(500):
         x = a + xx*(b - a)/500
-        if x == 0:
-            y_0 = xx
         y = func(x)
         if y < ymin:
             ymin = y
         if y > ymax:
             ymax = y
-    return ymin, ymax, y_0
+    return ymin, ymax
 
 
-def calculate(xx, a, b, miny, maxy, x_0):
+# вычисление у по х, масштабирование
+def calculate(xx, x_0):
     x = a + xx*(b - a)/500
     y = func(x)
-    yy = (y - maxy)*500/(miny - maxy)
+    yy = (y - ymax)*500/(ymin - ymax)
     if y == 0:
         x_0 = yy
     return yy, x_0
 
 
+# отрисовка осей
 def drow_axis(x, y):
     if x is not None:
         canvas.create_line(0, x, 500, x, fill="black", arrow=LAST)
@@ -39,26 +41,50 @@ def drow_axis(x, y):
         canvas.create_line(y, 500, y, 0, fill="black", arrow=LAST)
 
 
-def main(a, b):
-    x_axis_coord = None
-    miny, maxy, y_axis_coord = max_min(a, b)
-    yy = (func(a) - miny)*500/(maxy - miny)
+# поиск координваты х для отрисовки оси ОУ
+def find_ordinate_coord():
+    if a > 0:
+        return None
+    if a == 0:
+        return 0
+    for xx in range(500):
+        x = a + xx * (b - a) / 500
+        if x == 0:
+            return xx
+        if x > 0:
+            x_old = a + (xx - 1) * (b - a) / 500
+            if 0 - x_old < x:
+                return xx - 1
+            else:
+                return xx
+
+
+
+def main():
+    global ymin
+    global ymax
+    abscissa_coord = None
+    ymin, ymax = max_min()
+    yy = (func(a) - ymin)*500/(ymax - ymin)
     yy_old = yy
     for xx in range(0, 500):
-        yy, x_axis_coord = calculate(xx, a, b, miny, maxy, x_axis_coord)
+        yy, abscissa_coord = calculate(xx, abscissa_coord)
         canvas.create_line(xx, yy_old, xx + 1, yy, fill="#ff0000")
         yy_old = yy
-    drow_axis(x_axis_coord, y_axis_coord)
+    ordinate_coord = find_ordinate_coord()
+    drow_axis(abscissa_coord, ordinate_coord)
 
 
 if __name__ == "__main__":
+    global a
+    global b
     print("введите 2 числа:")
     a = int(input())
     b = int(input())
     root = Tk()
     canvas = Canvas(root, width=500, height=500, bg='white')
     canvas.pack()
-    main(a, b)
+    main()
     root.mainloop()
 
 
