da8f3cd13246c7ae9f8bd5b2993ff2ee1c035433
Masha1111 <sowix@yandex.ru>
2019-04-14 23:05:42 +0500

    OY done

--- firstTask.py
+++ firstTask.py
@@ -5,8 +5,9 @@ import math
 # вычисление значения функции
 def func(x):
     # return math.sin(x)
-    # return x*x
-    return x
+    return x*x
+    # return x
+    # return x*math.sin(x*x)
 
 
 # нахождение максимального и минимального значения функции
@@ -39,38 +40,39 @@ def calculate(xx, x_0, old_y):
 
 
 # отрисовка осей
-def drow_axis(x, y):
-    f = ymin
-    d = (round(x, 3))
+def drow_axis(x):
     if x is not None:
         canvas.create_line(0, x, 500, x, fill="black", arrow=LAST)
             # elif round(ymin, 2) == 0:
             #     canvas.create_line(0, x, 500, x, fill="black", arrow=LAST)
-    if y is not None:
-        canvas.create_line(y, 500, y, 0, fill="black", arrow=LAST)
 
 
-# поиск координаты х для отрисовки оси ОУ
+# поиск координаты х для отрисовки оси ОУ, отрисовка
 def find_ordinate_coord():
-    if a > 0:
-        return None
+    drawn_axis_OY = False
     if a == 0:
-        return 0
-    for xx in range(500):
-        x = a + xx * (b - a) / 500
-        if x == 0:
-            return xx
-        if x > 0:
-            x_old = a + (xx - 1) * (b - a) / 500
-            if 0 - x_old < x:
-                return xx - 1
-            else:
-                return xx
+        canvas.create_line(0, 500, 0, 0, fill="black", arrow=LAST)
+        drawn_axis_OY = True
+    else:
+        for xx in range(500):
+            x = a + xx * (b - a) / 500
+            if (x == 0) and (drawn_axis_OY is False):
+                canvas.create_line(xx, 500, xx, 0, fill="black", arrow=LAST)
+                drawn_axis_OY = True
+            if x > 0:
+                x_old = a + (xx - 1) * (b - a) / 500
+                if (0 - x_old < x) and (drawn_axis_OY is False):
+                    canvas.create_line(xx - 1, 500, xx - 1, 0, fill="black", arrow=LAST)
+                    drawn_axis_OY = True
+                elif drawn_axis_OY is False:
+                    canvas.create_line(xx, 500, xx, 0, fill="black", arrow=LAST)
+                    drawn_axis_OY = True
 
 
 def main():
     global ymin
     global ymax
+    drawn_abscissa = False;
     ymin, ymax = max_min()
     yy = (func(a) - ymin)*500/(ymax - ymin)
     yy_old = yy
@@ -82,7 +84,7 @@ def main():
         yy_old = yy
         y_old = old
     ordinate_coord = find_ordinate_coord()
-    drow_axis(abscissa_coord, ordinate_coord)
+    # drow_axis(abscissa_coord)
 
 
 if __name__ == "__main__":
